5a914fa104e54558e22a65d87efe66a8215dd3b6
Ndukwe Chima <chimandukwe.r@gmail.com>
2024-06-02 15:15:02 +0100

    Added footer

--- src/components/neighborhood/index.jsx
+++
@@ -1,9 +0,0 @@
-// import NotJustApp from "components/notJustApp";
-// import React from "react";
-// import Button from "shared/button";
-
-// const Neighborhood = () => {
-// return <section>{<NotJustApp /> }</section>;
-// };
-
-// export default Neighborhood;
---
+++ src/shared/footer/index.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import "./footerStyle.css";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaYoutube,
+} from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoCallOutline, IoLocationOutline } from "react-icons/io5";
+
+const Footer = () => {
+  return (
+    <section className="footer_Wrapper">
+      <section className="footer_container">
+        <section className="footer_section_one_div">
+          {/* left side */}
+          <div className="left_side_div">
+            <h5>Resida</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipiscing elit aliquam
+            </p>
+            <div className="social_media_div">
+              <FaFacebookF />
+              <FaXTwitter />
+              <FaInstagram />
+              <FaLinkedinIn />
+              <FaYoutube />
+            </div>
+          </div>
+          {/* right side */}
+          <div className="right_side_div">
+            <div className="right_div">
+              <h5>Products</h5>
+              <p>Pricing</p>
+              <p>Case studies</p>
+              <p>Reviews</p>
+              <p>Updates</p>
+            </div>
+            <div className="right_div">
+              <h5>Company</h5>
+              <p>Contact Us</p>
+              <p>Careers</p>
+              <p>Culture</p>
+              <p>Blog</p>
+            </div>
+            <div className="right_div">
+              <h5>Supports</h5>
+              <p>Help center</p>
+              <p>Server status</p>
+              <p> Report a bug</p>
+              <p>Chat support</p>
+            </div>
+            <div className="right_div">
+              <h5>Legal</h5>
+              <p>Privacy policy</p>
+              <p>User terms of use</p>
+              <p>Payment & pricing policy</p>
+              <p>Cookies & data privacy</p>
+            </div>
+          </div>
+        </section>
+
+        {/* middle section */}
+        <section className="middle_parent">
+          <div className="middle_div">
+            <div className="email_div">
+              <div>
+                <MdOutlineEmail size={16} />
+              </div>
+              <p>contact@company.com</p>
+            </div>
+            <div className="call_div">
+              <div>
+                <IoCallOutline size={16} />
+              </div>
+              <p>(414) 687 - 5892</p>
+            </div>
+            <div className="location_div">
+              <div>
+                <IoLocationOutline size={16} />
+              </div>
+              <p>794 Mcallister St San Francisco, 94102</p>
+            </div>
+          </div>
+        </section>
+
+        {/* Last section */}
+        <section className="last_section">
+          <hr />
+          <div className="last_section_div">
+            <p>Copyright © 2022 BRIX Templates</p>
+            <span>
+              All Rights Reserved | Terms and Conditions | Privacy Policy
+              <hr />
+            </span>
+          </div>
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default Footer;
